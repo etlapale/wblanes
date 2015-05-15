cc370d3e8e349cf3c05203f1a1cef3757125aa74
Émilien Tlapale <emilien@tlapale.com>
2015-05-14 19:01:38 -0700

    Storing the QML UI definition as qrc resource.

--- src/main.cc
+++ src/main.cc
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
   
   // Load the QML UI description
   auto eng = new QQmlApplicationEngine;
-  eng->load(QUrl::fromLocalFile("qml/wblanes.qml"));
+  eng->load(QUrl("qrc:/qml/wblanes.qml"));
 
   // Show the top level window
   auto win = qobject_cast<QQuickWindow*>(eng->rootObjects().value(0));
